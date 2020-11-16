7cdbe03c6cca0776f7fc1fb96f409f38f8838702
Ruben Requena <rubnrequena@gmail.com>
2020-11-17 00:59:52 +0100

    enviar elementoid para remover tope por lotes

--- src/dbfiles/topes.sql
+++ src/dbfiles/topes.sql
@@ -7,7 +7,7 @@ LEFT JOIN numeros ON numeros.elementoID = topes.elemento
 WHERE (topes.bancaID = :bancaID OR topes.bancaID = 0) AND (topes.usuarioID = :usuarioID OR topes.usuarioID = 0) 
 ORDER BY compartido DESC, elemento DESC, topes.sorteoID DESC, topes.taquillaID DESC, topes.bancaID DESC, topes.sorteo DESC, topeID DESC
 --topes_usuario
-SELECT creado, topes.usuarioID, topes.bancaID, topeID, monto, topes.taquillaID, taquillas.nombre "taquilla", sorteos.descripcion, topes.sorteo, topes.sorteoID, numeros.descripcion elemento, compartido 
+SELECT creado, topes.usuarioID, topes.bancaID, topeID, monto, topes.taquillaID, taquillas.nombre "taquilla", sorteos.descripcion, topes.sorteo, topes.sorteoID, numeros.descripcion elemento, numeros.elementoID num, compartido 
 FROM us.topes 
 LEFT JOIN us.taquillas ON taquillas.taquillaID = topes.taquillaID 
 LEFT JOIN vt.sorteos ON sorteos.sorteoID = topes.sorteoID 
